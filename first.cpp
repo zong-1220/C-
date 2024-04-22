041ac5eee4b09e03b08ae1252ed9bbbf069f637c
zong <daniel.chang1220@gmail.com>
2024-04-22 17:26:17 +0800

    寫了點東西

--- first.cpp
+++ first.cpp
@@ -1,6 +1,7 @@
 # include <bits/stdc++.h>
 int main() {
+    std::cout << "Hello world!" << std::endl;
+    // std::cout << "上面那個結尾有換行";
     std::cout << "hello world!";
-    std::cout << "hello world!";
-    std::cout << "hello world!";
+    // system("pause"); 可以不用會只是強迫.exe檔停留，日後使用迴圈就沒這個問題 
 }
